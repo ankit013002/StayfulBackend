ba365c68029ac08959c8131ce75009fbec310fd8
ankit013002 <112412889+ankit013002@users.noreply.github.com>
2025-07-11 15:46:16 -0400

    Created middleware and backend routes to validify user and both create and fetch user information based on jwt.

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import { authMiddleware } from "./middleware/authMiddleware";
+import tenantRoutes from "./routes/tenantRoutes";
+import managerRoutes from "./routes/managerRoutes";
 
 dotenv.config();
 const app = express();
@@ -18,6 +21,9 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Home Route");
 });
+
+app.use("/tenants", authMiddleware(["tenant"]), tenantRoutes);
+app.use("/managers", authMiddleware(["manager"]), managerRoutes);
 
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
